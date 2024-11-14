e088869a25d4028be320d6a0052234833a8bffbf
juniorfduarte <junior.fduarte@gmail.com>
2024-11-14 00:10:29 -0300

    starting project

---
+++ src/test/kotlin/br/com/juniorfduarte/spring_rest_kotlin/StartupTests.kt
@@ -0,0 +1,13 @@
+package br.com.juniorfduarte.spring_rest_kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
